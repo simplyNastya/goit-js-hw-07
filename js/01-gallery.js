5cadc01755f3d89123a75ed6e66ff5000dac5918
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-01-10 21:33:37 +0200

    Made gallery markup (task 1.1)
    Refactored code

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imagesContainerEl = document.querySelector('.gallery')
+
+imagesContainerEl.classList.add('gallery__item')
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+function createImagesMarkup (images) {
+    return images.map(({ description, original, preview }) => {
+        return `<a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src=${preview}
+          data-source=${original}
+          alt=${description}
+        />
+      </a>`
+    }).join('')
+}
+
+const imagesMarkup = createImagesMarkup(galleryItems)
+
+imagesContainerEl.insertAdjacentHTML('beforeend', imagesMarkup)
+
+
