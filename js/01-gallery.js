879c1b66841c056d3eed596a0d9401f3a5f75e3b
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-01-11 18:25:40 +0200

    Done 1 and 2 tests
    Refactored code

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,35 +1,41 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 const imagesContainerEl = document.querySelector('.gallery')
-
-imagesContainerEl.classList.add('gallery__item')
-
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-
 function createImagesMarkup (images) {
     return images.map(({ description, original, preview }) => {
-        return `<a class="gallery__link" href=${original}>
+        return `<div class="gallery__item">
+        <a class="gallery__link" href=${original}>
         <img
           class="gallery__image"
           src=${preview}
           data-source=${original}
           alt=${description}
         />
-      </a>`
+      </a>
+      </div>`
     }).join('')
 }
-
 const imagesMarkup = createImagesMarkup(galleryItems)
-
 imagesContainerEl.insertAdjacentHTML('beforeend', imagesMarkup)
 
-// const linkEl = document.querySelectorAll('.gallery__link')
-// linkEl.setAttribute("rel", "noopener noreferrer nofollow")
+const linkEl = document.querySelectorAll('.gallery__link')
+console.log(linkEl)
+const listLinkEl = [...linkEl]
+listLinkEl.map(e => e.setAttribute("rel", "noopener noreferrer nofollow"))
 
-// console.log(linkEl.hasAttribute('rel'))
+function onImagesContainerClick (event) {
+    event.preventDefault()
 
-// why TypeError: linkEl.hasAttribute is not a function??????
+    const imageEl = event.target
+    const isGalleryImageEl = event.target.classList.contains('gallery__image')
 
-
-
+    if (!isGalleryImageEl) {
+        return
+    } else {
+        const originalImg = event.target.dataset.source
+        basicLightbox.create(`<img src="${imageEl.src}" alt="${imageEl.description}"></img>`).show()
+    }
+}
+imagesContainerEl.addEventListener('click', onImagesContainerClick)
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imagesContainerEl = document.querySelector('.gallery')
+
+function createImagesMarkup (images) {
+    return images.map(({ description, original, preview }) => {
+        return `<a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt=${description} />
+      </a>`
+    }).join('')
+}
+
+const imagesMarkup = createImagesMarkup(galleryItems)
+
+imagesContainerEl.insertAdjacentHTML('beforeend', imagesMarkup)
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', 
+captionDelay: 250 });
+
+
+
+
+
